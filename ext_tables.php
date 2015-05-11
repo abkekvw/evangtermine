b4a435cdc9d9e87bac7c8aa61b104a93372dfbb3
Christoph Roth <christoph.roth@lka.ekvw.de>
2015-05-11 15:59:09 +0200

    configured flexforms

--- ext_tables.php
+++ ext_tables.php
@@ -10,3 +10,8 @@ if (!defined('TYPO3_MODE')) {
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Evangelische Termine');
+
+// Activate Flexforms
+$pluginSignature = 'evangtermine_evtermplugin';
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:evangtermine/Configuration/Flexforms/flexform_ds.xml');
