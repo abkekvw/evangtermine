273c4409bee01af53698eaa37c275ccf5cfcbfe0
Christoph Roth <christoph.roth@lka.ekvw.de>
2015-06-09 16:35:40 +0200

    added EtKeys class

---
+++ Tests/Unit/Domain/Model/EtKeysTest.php
@@ -0,0 +1,72 @@
+<?php
+namespace ArbkomEKvW\Evangtermine\Tests\Unit\Domain\Model;
+
+/**
+ * *************************************************************
+ * Copyright notice
+ *
+ * (c) 2015 Christoph Roth <christoph.roth@lka.ekvw.de>, Evangelische Kirche von Westfalen
+ *
+ * All rights reserved
+ *
+ * This script is part of the TYPO3 project. The TYPO3 project is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ * *************************************************************
+ */
+
+/**
+ * Test case for class \ArbkomEKvW\Evangtermine\Domain\Model\EtKeys.
+ *
+ * @copyright Copyright belongs to the respective authors
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ *
+ * @author Christoph Roth <christoph.roth@lka.ekvw.de>
+ */
+ class EtKeysTest extends \TYPO3\CMS\Core\Tests\UnitTestCase {
+ 	
+ 	/**
+ 	 * @var \ArbkomEKvW\Evangtermine\Domain\Model\EtKeys
+ 	 */
+ 	protected $subject = NULL;
+ 	
+ 	protected function setUp() {
+ 		// get a full fledged object manager as described in comment of GeneralUtility::makeInstance() of TYPO3 7.2
+ 		$objectManager = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('\TYPO3\CMS\Extbase\Object\ObjectManager');
+ 		$this->subject = $objectManager->get('\ArbkomEKvW\Evangtermine\Domain\Model\EtKeys');
+ 	}
+ 	
+ 	protected function tearDown() {
+ 		unset ($this->subject);
+ 	}
+ 	
+ 	/**
+ 	 * @test
+ 	 */
+ 	public function setSingleKey() {
+ 		$this->subject->setSingleKey('foo', 'theValue');
+ 		$this->subject->setSingleKey('q', 'param[Gewölk]');
+ 		$this->assertEquals('foo=theValue&q=param%5BGew%C3%B6lk%5D', $this->subject->getValue());
+ 	}
+ 	
+ 	/**
+ 	 * @test
+ 	 */
+ 	public function setAllKeys() {
+ 		$this->subject->setAllKeys(array('foo' => 'theValue', 'q' => 'param[Bär]'));
+ 		$this->assertEquals('foo=theValue&q=param%5BB%C3%A4r%5D', $this->subject->getValue());
+ 	}
+ 	
+ }
